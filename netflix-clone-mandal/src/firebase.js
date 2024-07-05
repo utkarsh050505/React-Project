22c68ebe289795c7ba8756acaef732f0ff28be00
Utkarsh Mandal <utkarshmandal420@gmail.com>
2024-07-05 17:03:11 +0530

    initial-commit

---
+++ netflix-clone-mandal/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA_r2nxjLKqsnwP_ljc98aPBqaK86IEZ24",
+    authDomain: "netflix-clone-mandal.firebaseapp.com",
+    projectId: "netflix-clone-mandal",
+    storageBucket: "netflix-clone-mandal.appspot.com",
+    messagingSenderId: "127844263473",
+    appId: "1:127844263473:web:f70c6404cde93611aed710"
+  };

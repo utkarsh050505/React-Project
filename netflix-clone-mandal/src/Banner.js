7dcbf3daabc0f66d883e24048a09320ced9d4269
Utkarsh Mandal <utkarshmandal420@gmail.com>
2024-07-08 10:52:05 +0530

    banner-done

--- netflix-clone-mandal/src/Banner.js
+++ netflix-clone-mandal/src/Banner.js
@@ -1,7 +1,22 @@
 import React from "react";
 import './Banner.css';
+import axios from './axios';
+import requests from "./Requests";
 
 function Banner() {
+
+    const [movie, setMovie] = React.useState([])
+
+    React.useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+                request.data.results[Math.floor(Math.random() * request.data.results.length - 1)]
+            );
+            return request;
+        }
+        fetchData();
+    }, [])
 
     function truncateString(string, n) {
         return string?.length > n ? string.substr(0, n - 1) + "...": string
@@ -10,19 +25,19 @@ function Banner() {
     return (
         <header className="banner" style={{
             backgroundSize: "cover",
-            backgroundImage: `url('https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png')`,
+            backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`,
             backgroundPosition: "center center"
         }}>
             <div className="banner__contents">
                 <h1 className="banner__title">
-                    Movie Name
+                    {movie?.title || movie?.original_name || movie?.name}
                 </h1>
                 <div className="banner__buttons">
                     <button className="banner__button">Play</button>
                     <button className="banner__button">My List</button>
                 </div>
                 <h1 className="banner__description">
-                    {truncateString(`Test Description`, 150)}
+                    {truncateString(movie?.overview, 150)}
                 </h1>
             </div>
             <div className="banner--fadeBottom" />

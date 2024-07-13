8c78addf13d3f7c03bd4f595ea8718a75aac492a
Utkarsh Mandal <utkarshmandal420@gmail.com>
2024-07-13 19:43:01 +0530

    Created-SignIn

--- netflix-clone-mandal/src/screens/LoginScreen.js
+++ netflix-clone-mandal/src/screens/LoginScreen.js
@@ -12,6 +12,21 @@ function LoginScreen() {
              <button className='loginScreen__button'>
                 Sign In
              </button>
+             <div className='loginScreen__gradient' />
+        </div>
+        <div className='loginScreen__body'>
+          <>
+          <h1>Unlimited movies, TV programmes and more.</h1>
+          <h2>Watch anywhere. Cancel at anytime.</h2>
+          <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+
+          <div className='loginScreen__input'>
+            <form>
+              <input type="email" placeholder='Email Address'/>
+              <button className='loginScreen__getStarted'>GET STARTED</button>
+            </form>
+          </div>
+          </>
         </div>
         </div>
   )

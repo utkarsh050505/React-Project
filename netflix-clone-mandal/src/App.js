b59e1bd50e40adb959521eb9c5995c55bf24ecc7
Utkarsh Mandal <utkarshmandal420@gmail.com>
2024-07-20 11:31:51 +0530

    Add-EditProfile

--- netflix-clone-mandal/src/App.js
+++ netflix-clone-mandal/src/App.js
@@ -1,51 +1,48 @@
 import React from 'react';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
-import { BrowserRouter as Router, Switch , Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginScreen from './screens/LoginScreen';
+import ProfileScreen from './screens/ProfileScreen';
 import { auth } from './firebase';
 import { useDispatch, useSelector } from 'react-redux';
 import { logout, login, selectUser } from './features/userSlice';
 
 function App() {
- const user = useSelector(selectUser);
-  console.log(user);
-  const dispatch = useDispatch();
+    const user = useSelector(selectUser);
+    const dispatch = useDispatch();
 
-  React.useEffect(() => {
-    const unsub = auth.onAuthStateChanged((userAuth) => {
-      if (userAuth) {
-        dispatch(
-          login({
-            uid: userAuth.uid,
-            email: userAuth.email,
-          })
-        );
-      } else {
-        dispatch(logout());
-      }
-    });
+    React.useEffect(() => {
+        const unsub = auth.onAuthStateChanged((userAuth) => {
+            if (userAuth) {
+                dispatch(
+                    login({
+                        uid: userAuth.uid,
+                        email: userAuth.email,
+                    })
+                );
+            } else {
+                dispatch(logout());
+            }
+        });
 
-    return () => unsub(); // Clean up the subscription on unmount
-  }, [dispatch]); // Add dispatch as a dependency
+        return () => unsub(); // Clean up the subscription on unmount
+    }, [dispatch]); // Add dispatch as a dependency
 
-  return (
-    <div className="app">
-      <Router>
-        {!user ?
-        (
-          <LoginScreen />
-        )
-        :
-        (<Switch>
-          <Route path="/">
-          <HomeScreen />
-          </Route>
-        </Switch>)
-        }   
-      </Router>
-    </div>
-  );
+    return (
+        <div className="app">
+            <Router>
+                {!user ? (
+                    <LoginScreen />
+                ) : (
+                    <Routes>
+                        <Route path="/profile" element={<ProfileScreen />} />
+                        <Route path="/" element={<HomeScreen />} />
+                    </Routes>
+                )}
+            </Router>
+        </div>
+    );
 }
 
 export default App;
--- netflix-clone-mandal/src/Nav.js
+++ netflix-clone-mandal/src/Nav.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './Nav.css';
+import { useNavigate } from 'react-router-dom';
 
 function Nav() {
+
+    const navigate = useNavigate();
 
     const [show, handleShow] = React.useState(false)
 
@@ -23,12 +26,16 @@ function Nav() {
         <div className={`nav ${show && 'nav__black'}`}>
             <div className='nav__contents'>
             <img
+            onClick={() => navigate('/')}
             className='nav__logo'
             src='https://img.icons8.com/?size=100&id=20519&format=png&color=000000'
             //src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQj0jWPJ2dABPXDnURT5UrGTNzA5ONHfTO2qQ&s"
             alt="" />
 
             <img
+            onClick={() => {
+                navigate('/profile')
+            }}
             className='nav__avatar' 
             src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
             alt="" />

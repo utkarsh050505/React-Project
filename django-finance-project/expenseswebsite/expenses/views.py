7bc4fc65bd93f37b055be19058460b9ba571a4fc
Utkarsh Mandal <utkarshmandal420@gmail.com>
2024-07-21 12:48:55 +0530

    test

--- django-finance-project/expenseswebsite/expenses/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render # type: ignore
-
-# Create your views here.
-def index(request):
-    return render(request, 'expenses/index.html')
-
-def add_expense(request):
-    return render(request, 'expenses/add_expense.html')
